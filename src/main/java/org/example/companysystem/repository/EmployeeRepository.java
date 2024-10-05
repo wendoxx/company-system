70a6e09f17b66ed6ebf70f5be16c7d3ed95ff76d
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-05 17:37:25 -0300

    fix: correct parameter

--- src/main/java/org/example/companysystem/repository/EmployeeRepository.java
+++ src/main/java/org/example/companysystem/repository/EmployeeRepository.java
@@ -1,13 +1,16 @@
 package org.example.companysystem.repository;
 
+import org.example.companysystem.model.EmployeeModel;
+import org.example.companysystem.model.EmployeeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeRepository, UUID> {
-    EmployeeRepository findByName(String name);
-    EmployeeRepository findByFunction(String function);
-    EmployeeRepository findByStatus(String status);
+public interface EmployeeRepository extends JpaRepository<EmployeeModel, UUID> {
+    Optional<EmployeeRepository> findByName(String name);
+    Optional<EmployeeRepository> findByFunction(String function);
+    Optional<EmployeeRepository> findByStatus(String status);
 }
