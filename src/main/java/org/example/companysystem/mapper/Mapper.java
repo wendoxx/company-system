fd7ecb846c1f575e4fe8bb3bf0acf64c7e713d10
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-05 17:36:43 -0300

    feat: add annotation @Bean in Mapper

--- src/main/java/org/example/companysystem/mapper/Mapper.java
+++ src/main/java/org/example/companysystem/mapper/Mapper.java
@@ -1,10 +1,12 @@
 package org.example.companysystem.mapper;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Mapper {
+    @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
