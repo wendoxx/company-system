6466af054518f25c74f4ef9677743c81197f1666
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-04 21:34:59 -0300

    add spring project

---
+++ src/test/java/org/example/companysystem/CompanySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.companysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
