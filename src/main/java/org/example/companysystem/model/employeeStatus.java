3afdfb74e942601647bca7771705453990fcf81d
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-04 22:35:46 -0300

    feat: add employee status

---
+++ src/main/java/org/example/companysystem/model/employeeStatus.java
@@ -0,0 +1,11 @@
+package org.example.companysystem.model;
+
+public enum employeeStatus {
+    ACTIVE,
+    INACTIVE,
+    VACATION,
+    SICK_LEAVE,
+    MATERNITY_LEAVE,
+    PATERNITY_LEAVE,
+    SUSPENDED
+}
